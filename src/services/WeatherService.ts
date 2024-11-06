4d696a82b9fc1db3eb1214ca766937a2bf9a2001
austinb847 <austinb847@icloud.com>
2024-11-05 22:24:28 -0600

    feat: add new weather service method

--- src/services/WeatherService.ts
+++ src/services/WeatherService.ts
@@ -46,18 +46,88 @@ export type WeatherResponse = {
   cod: number
 }
 
+export type ForecastResponse = {
+  city: {
+    id: number
+    name: string
+    coord: {
+      lat: number
+      lon: number
+    }
+    country: string
+    population: number
+    timezone: number
+    sunrise: number
+    sunset: number
+  }
+  cnt: number
+  list: {
+    dt: number
+    main: {
+      temp: number
+      feels_like: number
+      temp_min: number
+      temp_max: number
+      pressure: number
+      sea_level: number
+      grnd_level: number
+      humidity: number
+      temp_kf: number
+    }
+    weather: {
+      id: number
+      main: string
+      description: string
+      icon: string
+    }[]
+    clouds: {
+      all: number
+    }
+    wind: {
+      speed: number
+      deg: number
+      gust: number
+    }
+    visibility: number
+    pop: number
+    rain?: {
+      '3h': number
+    }
+    sys: {
+      pod: 'd' | 'n'
+    }
+    dt_txt: string
+  }[]
+}
+
 type units = 'metric' | 'imperial' | 'standard'
 
 class WeatherService {
-  private static baseWeatherUrl =
-    'https://api.openweathermap.org/data/2.5/weather'
+  private static baseWeatherUrl = 'https://api.openweathermap.org/data/2.5'
 
   static async getCurrentWeather(
     city: string,
     units: units = 'metric'
   ): Promise<WeatherResponse> {
     const response = await axios.get<WeatherResponse>(
-      `${this.baseWeatherUrl}`,
+      `${this.baseWeatherUrl}/weather`,
+      {
+        params: {
+          q: city,
+          appid: API_KEY,
+          units
+        }
+      }
+    )
+    return response.data
+  }
+
+  static async getFiveDayForecast(
+    city: string,
+    units: units = 'metric'
+  ): Promise<ForecastResponse> {
+    const response = await axios.get<ForecastResponse>(
+      `${this.baseWeatherUrl}/forecast`,
       {
         params: {
           q: city,
