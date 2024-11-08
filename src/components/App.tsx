be04cfefc40edbb38b8ac05f3c1c8dd38d0bebf9
austinb847 <austinb847@icloud.com>
2024-11-07 21:41:56 -0600

    feat: create base app component

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,66 +1,74 @@
-import Avatar from 'components/Avatar'
-import logo from 'assets/logo.svg'
-
-const randoms = [
-  [1, 2],
-  [3, 4, 5],
-  [6, 7]
-]
+import { useMemo, useState } from 'react'
+import useCurrentWeather from '../hooks/useCurrentWeather'
+import useFiveDayForecast from '../hooks/useFiveDayForecast'
+import SearchBar from './SearchBar/SearchBar'
+import CurrentWeatherCard, {
+  CurrentWeatherCardSkeleton
+} from './CurrentWeatherCard/CurrentWeatherCard'
+import ForecastCards, {
+  ForecastCardsSkeleton
+} from './ForecastCards/ForecastCards'
+import { Unit } from './TemperatureToggle/TemperatureToggle'
 
 function App() {
+  const [searchCity, setSearchCity] = useState<string>('')
+  const [temperatureUnit, setTemperatureUnit] = useState<Unit>('C')
+  const { currentWeatherData, loading, error } = useCurrentWeather(searchCity)
+  const {
+    forecastData,
+    loading: forecastLoading,
+    error: forecastError
+  } = useFiveDayForecast(searchCity)
+
+  const dateText = new Date().toLocaleDateString('en-US', {
+    weekday: 'long',
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric'
+  })
+
+  const dailyForecasts = useMemo(() => {
+    return forecastData?.list
+      .filter((forecast) => forecast.dt_txt.includes('12:00:00'))
+      .slice(0, 5)
+  }, [forecastData])
+
+  const handleSearch = (city: string) => setSearchCity(city)
+
+  const hasError = error || forecastError
+
   return (
-    <div className="relative overflow-hidden bg-white">
-      <div className="h-screen sm:pb-40 sm:pt-24 lg:pb-48 lg:pt-40">
-        <div className="relative mx-auto max-w-7xl px-4 sm:static sm:px-6 lg:px-8">
-          <div className="sm:max-w-lg">
-            <div className="my-4">
-              <Avatar size="large" src={logo} />
-            </div>
-            <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
-              Welcome!
-            </h1>
-            <p className="mt-4 text-xl text-gray-500">
-              This is a boilerplate build with Vite, React 18, TypeScript,
-              Vitest, Testing Library, TailwindCSS 3, Eslint and Prettier.
-            </p>
-          </div>
-          <div className="my-10">
-            <a
-              href="vscode://"
-              className="inline-block rounded-md border border-transparent bg-indigo-600 px-8 py-3 text-center font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-700 focus:ring-offset-2"
-            >
-              Start building for free
-            </a>
-            <div
-              aria-hidden="true"
-              className="pointer-events-none mt-10 md:mt-0 lg:absolute lg:inset-y-0 lg:mx-auto lg:w-full lg:max-w-7xl"
-            >
-              <div className="absolute sm:left-1/2 sm:top-0 sm:translate-x-8 lg:left-1/2 lg:top-1/2 lg:-translate-y-1/2 lg:translate-x-8">
-                <div className="flex items-center space-x-6 lg:space-x-8">
-                  {randoms.map((random, number) => (
-                    <div
-                      key={`random-${random[number]}`}
-                      className="grid shrink-0 grid-cols-1 gap-y-6 lg:gap-y-8"
-                    >
-                      {random.map((number) => (
-                        <div
-                          key={`random-${number}`}
-                          className="h-64 w-44 overflow-hidden rounded-lg sm:opacity-0 lg:opacity-100"
-                        >
-                          <img
-                            src={`https://picsum.photos/600?random=${number}`}
-                            alt=""
-                            className="size-full bg-indigo-100 object-cover object-center"
-                          />
-                        </div>
-                      ))}
-                    </div>
-                  ))}
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
+    <div className="flex min-h-screen items-start justify-center bg-gradient-to-br from-gray-100 via-blue-50 to-gray-200 px-4 pt-10 md:px-8">
+      <div className="font-poppins flex w-full max-w-lg flex-col items-center rounded-lg bg-indigo-50 p-6 text-center shadow-2xl md:p-8">
+        <SearchBar onSearch={handleSearch} />
+        {hasError ? (
+          <p className="text-red-500">Failed to fetch weather data</p>
+        ) : (
+          <>
+            {loading ? (
+              <CurrentWeatherCardSkeleton />
+            ) : (
+              currentWeatherData && (
+                <CurrentWeatherCard
+                  data={currentWeatherData}
+                  temperatureUnit={temperatureUnit}
+                  onToggleUnit={setTemperatureUnit}
+                  dateText={dateText}
+                />
+              )
+            )}
+            {forecastLoading ? (
+              <ForecastCardsSkeleton />
+            ) : (
+              dailyForecasts && (
+                <ForecastCards
+                  forecasts={dailyForecasts}
+                  temperatureUnit={temperatureUnit}
+                />
+              )
+            )}
+          </>
+        )}
       </div>
     </div>
   )
